603c06208719564b6152950cc88c5d32e502fb4b
jpmachado2000 <jose.machado@ipvc.pt>
2021-06-21 17:41:44 +0100

    backend initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 /**
  * Need to be modulirized later
