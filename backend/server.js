e9f252b9aced5dbd2648359d4aa45cc15ab156cd
jpmachado2000 <jose.machado@ipvc.pt>
2021-06-21 19:13:59 +0100

    Created Benefit Endpoints (create and get)

--- backend/server.js
+++ backend/server.js
@@ -1007,4 +1007,137 @@ app.get("/get_all_SOLIDARITYINST", async (req, res) => {
     });
 });
 
+//TODO: BENEFITY ENDPOINTS
+//Create Benefit
+app.get("/create_BENEFIT", async (req, res) => {
+  let address = TP_NAMESPACE + _hash("sampleKey").substr(0, 64);
+
+  const payload = {
+    id: uuidv4(),
+    date: Date(Date.now()),
+    description: "Fundos adquiridos no evento da cadeira de rodas",
+    amount: 4345.75,
+    dataType: {
+      type: "BENEFIT",
+      subType: "BENEFIT",
+    },
+  };
+
+  // Input for one transaction
+  const payloadBytes = Buffer.from(JSON.stringify(payload));
+
+  // Output we created with this transaction input
+
+  const transactionHeaderBytes = protobuf.TransactionHeader.encode({
+    familyName: TP_FAMILY,
+    familyVersion: TP_VERSION,
+    // Needs to be same as the expected address we create in contract
+    // If diffrent we wont get access to put state and get state of the address
+    inputs: [address],
+    outputs: [address],
+    signerPublicKey: signer.getPublicKey().asHex(),
+    batcherPublicKey: signer.getPublicKey().asHex(),
+    dependencies: [],
+    payloadSha512: createHash("sha512").update(payloadBytes).digest("hex"),
+  }).finish();
+
+  const signature = signer.sign(transactionHeaderBytes);
+
+  // Sign the transaction
+  const transaction = protobuf.Transaction.create({
+    header: transactionHeaderBytes,
+    headerSignature: signature,
+    payload: payloadBytes,
+  });
+
+  // Wrap it into list of transaction
+  const transactions = [transaction];
+
+  const batchHeaderBytes = protobuf.BatchHeader.encode({
+    signerPublicKey: signer.getPublicKey().asHex(),
+    transactionIds: transactions.map((txn) => txn.headerSignature),
+  }).finish();
+
+  // Wrap the transaction list into batch
+  const batchSignature = signer.sign(batchHeaderBytes);
+
+  // And sign it
+  const batch = protobuf.Batch.create({
+    header: batchHeaderBytes,
+    headerSignature: batchSignature,
+    transactions: transactions,
+  });
+
+  // Wrap them in batch list
+  const batchListBytes = protobuf.BatchList.encode({
+    batches: [batch],
+  }).finish();
+  axios
+    .post(`${API_URL}/batches`, batchListBytes, {
+      headers: { "Content-Type": "application/octet-stream" },
+    })
+    .then((response) => {
+      console.log({
+        address,
+        TP_NAMESPACE,
+      });
+      console.log(response.data);
+
+      res.send({
+        message: "submitted",
+        data: response.data,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send({
+        message: "submitted",
+        error: error.response.data,
+      });
+    });
+});
+
+//Get Benefits
+app.get("/get_all_BENEFITS", async (req, res) => {
+  axios
+    .get(`${API_URL}/transactions`)
+    .then((response) => {
+      return response.data.data;
+    })
+    .then((data) => {
+      let resPayloads = [];
+      data.forEach((element) => {
+        if (
+          element != data[data.length - 1] &&
+          element != data[data.length - 2] &&
+          element != data[data.length - 3]
+        ) {
+          let payload = element.payload;
+
+          let decodedPayload = Buffer.from(payload, "base64").toString("utf-8");
+          console.log(
+            decodedPayload +
+              "\n---------------------------------------------------------------------"
+          );
+
+          decodedPayload = JSON.parse(decodedPayload);
+
+          if (decodedPayload.dataType.type === "BENEFIT") {
+            resPayloads.push(decodedPayload);
+          }
+        }
+      });
+
+      console.log(
+        "--------------------------- PAYLOADS ARRAY --------------------------\n" +
+          JSON.stringify(resPayloads) +
+          "\n---------------------------------------------------------------------\n"
+      );
+      res.send(resPayloads);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
 app.listen(8080, () => console.log("Server started"));
